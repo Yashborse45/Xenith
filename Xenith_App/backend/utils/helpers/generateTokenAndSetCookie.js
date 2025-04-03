2d0206a7355c014d51bcc73570661bb391d9dabf
Yashborse45 <yash.praful.borse@gmail.com>
2025-04-03 13:08:18 +0530

    Made some changes 2

--- Xenith_App/backend/utils/helpers/generateTokenAndSetCookie.js
+++ Xenith_App/backend/utils/helpers/generateTokenAndSetCookie.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
+		expiresIn: "30m",
 	});
 
 	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
-		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+		maxAge: 30 * 60 * 1000,
 		sameSite: "strict", // CSRF
 	});
 
